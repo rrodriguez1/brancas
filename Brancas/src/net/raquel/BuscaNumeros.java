65c6a46ae563b254c0537a62d05b3b6177081cbb
raquel <raquel@raquel-Aspire-5610>
2014-10-23 21:03:29 +0200

    SUBIR JAVA

---
+++ Brancas/src/net/raquel/BuscaNumeros.java
@@ -0,0 +1,32 @@
+package net.raquel;
+
+import java.util.Scanner;
+
+public class BuscaNumeros {
+	public static void main(final String[] args) {
+	long TInicio, TFin, tiempo;
+	TInicio = System.currentTimeMillis();
+	Scanner lector = new Scanner(System.in);
+	System.out.print("ELIGE NUMERO: ");
+	int numero = lector.nextInt();
+	boolean encontrado = false;
+	int i = 0;
+
+	while( i < 500000 ){
+		if (numero == i){
+			encontrado = true;
+			break;
+		}
+		i++;
+	}
+	
+	if (encontrado == true){
+		System.out.print("ENCONTRADO!");
+	}else{
+		System.out.print("NO ENCONTRADO!");
+	}
+	TFin = System.currentTimeMillis();
+	tiempo = TFin - TInicio;
+	System.out.println("Tiempo de ejecución en milisegundos: " + tiempo + " milisegundos.");
+}
+}
