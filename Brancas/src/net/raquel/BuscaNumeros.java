4cdf960164b2a28f411f6ed32f5a446859090753
raquel <raquel@raquel-Aspire-5610>
2014-10-23 22:34:16 +0200

    nuevo commit

--- Brancas/src/net/raquel/BuscaNumeros.java
+++ Brancas/src/net/raquel/BuscaNumeros.java
@@ -1,7 +1,7 @@
 package net.raquel;
 
 import java.util.Scanner;
-//MODIFICADO!!!!
+//MODIFICADO!!!!otra vez!!!
 public class BuscaNumeros {
 	public static void main(final String[] args) {
 	long TInicio, TFin, tiempo;
